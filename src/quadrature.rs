f07596ed0700d04b58991347637becb5eccabc1b
Rafael Pinto <rafaelgcpp@gmail.com>
2026-04-10 14:39:40 -0300

    adding unity test

--- src/quadrature.rs
+++ src/quadrature.rs
@@ -34,3 +34,30 @@ where
     let c2 = (b + a) / 2.0;
     c1 * gauss_quadrature(|x| f(c1 * x + c2))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_quadrature_gauss_quadrature() {
+        // integral of f(x) = cos(x* pi/2) * pi/2 in [-1, 1] should be 2.0
+        let result = gauss_quadrature(|x| (x * std::f64::consts::PI / 2.0).cos() * std::f64::consts::PI / 2.0);
+        assert!((result - 2.0).abs() < 1e-4);
+
+        //integral of f(x) = x^2 in [-1, 1] should be 2/3
+        let result = gauss_quadrature(|x| x * x);
+        assert!((result - 2.0 / 3.0).abs() < 1e-4);
+    }
+
+    #[test]
+    fn test_quadrature_integrate() {
+        // integral of f(x) = x in [-1, 1] should be 0.0
+        let result = integrate(|x| x, -1.0, 1.0);
+        assert!(result.abs() < 1e-4);
+
+        // integral of f(x) = x^2 in [0, 3] should be 9.0
+        let result = integrate(|x| x * x, 0.0, 3.0);
+        assert!((result - 9.0).abs() < 1e-4);
+    }
+}
