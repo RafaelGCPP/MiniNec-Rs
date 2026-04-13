dde95c35ca1940f678205ec8a7bbc3fd5f403973
Rafael Gustavo da Cunha Pereira Pinto <rafaelgcpp@gmail.com>
2026-04-13 07:42:32 -0300

    refactor: update antenna data structure and add file reading functionality

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-mod geometry;
+mod geometry_file;
 mod integrate;
+
+use geometry_file::*;
 
 fn main() {
     println!("Hello, world!");
 
-    let antenna = geometry::read_antenna_from_file("TestData/antenna.json");
+    let antenna = read_antenna_from_file("TestData/antenna.json");
     println!("{:#?}", antenna);
     
 }
+
