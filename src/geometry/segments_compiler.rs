0b3efa86f41937121ade36dac679dfbef912eb8b
Rafael Gustavo da Cunha Pereira Pinto <rafaelgcpp@gmail.com>
2026-04-13 15:09:51 -0300

    Adding segment radius and positional data

--- src/geometry/segments_compiler.rs
+++ src/geometry/segments_compiler.rs
@@ -1,8 +1,8 @@
-use std::collections::HashMap;
-use approx::relative_eq;
 use super::AntennaFile;
-use nalgebra::Point3;
+use approx::relative_eq;
+use nalgebra::{Point3, Vector3};
 use physical_constants;
+use std::collections::HashMap;
 
 /// An antenna node composed by its coordinates and incidence
 #[derive(Clone, Copy, Debug)]
@@ -35,7 +35,14 @@ pub struct Antenna {
     pub wire_map: HashMap<String, WireMetadata>,
 }
 
-type Segment = (usize, usize);
+#[derive(Clone, Debug)]
+pub struct Segment {
+    pub nodes: (usize, usize),
+    pub midpoint: Point3<f64>,
+    pub radius: f64,
+    pub length: f64,
+    pub unit_vector: Vector3<f64>,
+}
 
 const C0: f64 = physical_constants::SPEED_OF_LIGHT_IN_VACUUM;
 
@@ -60,7 +67,6 @@ fn push_node(p: Point3<f64>, nodes: &mut Vec<Node>) -> usize {
     }
 }
 
-
 /// Segments a line between two points into smaller segments based on the target segment size.
 ///
 /// # Parameters
@@ -75,6 +81,7 @@ fn push_node(p: Point3<f64>, nodes: &mut Vec<Node>) -> usize {
 fn segment_line(
     p1: Point3<f64>,
     p2: Point3<f64>,
+    radius: f64,
     target_size: f64,
     nodes: &mut Vec<Node>,
     segments: &mut Vec<Segment>,
@@ -99,7 +106,16 @@ fn segment_line(
         let idx_a = push_node(p_a, nodes);
         let idx_b = push_node(p_b, nodes);
 
-        segments.push((idx_a, idx_b));
+        let s_vector=p_b-p_a;
+        let segment = Segment {
+            nodes: (idx_a,idx_b),
+            midpoint: nalgebra::center(&p_a, &p_b),
+            radius,
+            length: s_vector.norm(),
+            unit_vector: s_vector.normalize(),
+        };
+
+        segments.push(segment);
 
         // Captures the first node
         if i == 0 {
@@ -121,7 +137,7 @@ fn segment_line(
 ///
 /// # Returns
 /// An `Antenna` struct containing the nodes, segments, and wire metadata.
-fn compile_geometry_file(file: &AntennaFile, segment_size_divider:f64) -> Antenna {
+fn compile_geometry_file(file: &AntennaFile, segment_size_divider: f64) -> Antenna {
     let mut nodes = Vec::new();
     let mut segments = Vec::new();
     let mut wire_map = HashMap::new();
@@ -134,76 +150,110 @@ fn compile_geometry_file(file: &AntennaFile, segment_size_divider:f64) -> Antenn
     for wire in &file.wires {
         let p_start = Point3::new(wire.start.x, wire.start.y, wire.start.z + height);
         let p_end = Point3::new(wire.end.x, wire.end.y, wire.end.z + height);
-        let midpoint=nalgebra::center(&p_start,&p_end);
-        let first_half= segment_line(p_start, midpoint, segment_size, &mut nodes, &mut segments);
-        let second_half = segment_line(midpoint, p_end, segment_size, &mut nodes, &mut segments);
+        let midpoint = nalgebra::center(&p_start, &p_end);
+        let radius = wire.diameter / 2.0;
+        let first_half = segment_line(
+            p_start,
+            midpoint,
+            radius,
+            segment_size,
+            &mut nodes,
+            &mut segments,
+        );
+        let second_half = segment_line(
+            midpoint,
+            p_end,
+            radius,
+            segment_size,
+            &mut nodes,
+            &mut segments,
+        );
         let wire_metadata = WireMetadata {
             first_node: first_half.0,
             middle_node: first_half.1, // should be the same as second_half.0
-            last_node: second_half.1
+            last_node: second_half.1,
         };
-        println!("Inserting wire: id = {:?}, metadata = {:?}", wire.id, wire_metadata);
+        println!(
+            "Inserting wire: id = {:?}, metadata = {:?}",
+            wire.id, wire_metadata
+        );
         wire_map.insert(wire.id.clone(), wire_metadata);
         println!("Current wire_map state:");
-        for (k,v) in wire_map.iter() {
+        for (k, v) in wire_map.iter() {
             println!("  id = {:?}, metadata = {:?}", k, v);
         }
     }
 
-    Antenna { nodes, segments, wire_map }
+    Antenna {
+        nodes,
+        segments,
+        wire_map,
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::geometry::read_antenna_from_file;
     use super::*;
+    use crate::geometry::read_antenna_from_file;
 
     #[test]
     fn test_compile_geometry_file_dipole() {
         let file = read_antenna_from_file("TestData/antenna.json").unwrap();
-        let antenna=compile_geometry_file(&file,20.0);
+        let antenna = compile_geometry_file(&file, 20.0);
 
-        assert_eq!(antenna.wire_map.len(),1); // There is only one wire in the test file
-        assert_eq!(antenna.nodes.len(),13); // There should be 13 nodes
-        assert_eq!(antenna.segments.len(),12); // There should be 13 segments
-        assert_eq!(antenna.nodes[0].incidence,1); // the first node is open
-        assert_eq!(antenna.nodes[12].incidence,1); // the last node is open
+        assert_eq!(antenna.wire_map.len(), 1); // There is only one wire in the test file
+        assert_eq!(antenna.nodes.len(), 13); // There should be 13 nodes
+        assert_eq!(antenna.segments.len(), 12); // There should be 13 segments
+        assert_eq!(antenna.nodes[0].incidence, 1); // the first node is open
+        assert_eq!(antenna.nodes[12].incidence, 1); // the last node is open
 
         // nodes 1..11 must have incidence 2
         for i in 1..11 {
-            assert_eq!(antenna.nodes[i].incidence, 2, "Node {} has incidence {}, expected 2", i, antenna.nodes[i].incidence);
+            assert_eq!(
+                antenna.nodes[i].incidence, 2,
+                "Node {} has incidence {}, expected 2",
+                i, antenna.nodes[i].incidence
+            );
         }
     }
 
     #[test]
     fn test_compile_geometry_file_folded_dipole() {
         let file = read_antenna_from_file("TestData/folded.json").unwrap();
-        let antenna=compile_geometry_file(&file,20.0);
+        let antenna = compile_geometry_file(&file, 20.0);
 
-        assert_eq!(antenna.wire_map.len(),4); // There is 4 wires in the test file
-        assert_eq!(antenna.nodes.len(),28); // There should be 28 nodes
-        assert_eq!(antenna.segments.len(),28); // There should be 28 segments
+        assert_eq!(antenna.wire_map.len(), 4); // There is 4 wires in the test file
+        assert_eq!(antenna.nodes.len(), 28); // There should be 28 nodes
+        assert_eq!(antenna.segments.len(), 28); // There should be 28 segments
         for i in 0..27 {
-            assert_eq!(antenna.nodes[i].incidence,2, "Node {} has incidence {}, expected 2", i, antenna.nodes[i].incidence);
+            assert_eq!(
+                antenna.nodes[i].incidence, 2,
+                "Node {} has incidence {}, expected 2",
+                i, antenna.nodes[i].incidence
+            );
         }
     }
     #[test]
     fn test_compile_geometry_file_vertical() {
         let file = read_antenna_from_file("TestData/vertical.json").unwrap();
-        let antenna=compile_geometry_file(&file,20.0);
+        let antenna = compile_geometry_file(&file, 20.0);
 
-        assert_eq!(antenna.wire_map.len(),5); // There is 4 wires in the test file
-        assert_eq!(antenna.nodes.len(),23); // There should be 28 nodes
-        assert_eq!(antenna.segments.len(),22); // There should be 28 segments
+        assert_eq!(antenna.wire_map.len(), 5); // There is 4 wires in the test file
+        assert_eq!(antenna.nodes.len(), 23); // There should be 28 nodes
+        assert_eq!(antenna.segments.len(), 22); // There should be 28 segments
         for i in 0..23 {
             let mut incidence = 2;
-            if i==0 {
-                incidence=5;
+            if i == 0 {
+                incidence = 5;
             }
-            if (i==6) || (i==10) || (i==14) || (i==18) || (i==22) {
-                incidence=1;
+            if (i == 6) || (i == 10) || (i == 14) || (i == 18) || (i == 22) {
+                incidence = 1;
             }
-            assert_eq!(antenna.nodes[i].incidence,incidence, "Node {} has incidence {}, expected 2", i, antenna.nodes[i].incidence);
+            assert_eq!(
+                antenna.nodes[i].incidence, incidence,
+                "Node {} has incidence {}, expected 2",
+                i, antenna.nodes[i].incidence
+            );
         }
     }
 }
