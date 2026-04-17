52fdfc2edad607ff0a3d70202c1795269baea88c
Rafael Gustavo da Cunha Pereira Pinto <rafaelgcpp@gmail.com>
2026-04-17 20:31:43 -0300

    preparing pulse compiler

--- src/geometry/pulse_compiler.rs
+++ src/geometry/pulse_compiler.rs
@@ -6,9 +6,32 @@ pub fn compile_pulses(antenna: &Antenna) -> Vec<Pulse> {
     let nodes = &antenna.nodes;
 
     for (i, node) in nodes.iter().enumerate() {
-        if node.segments.len()<2 {
-            continue; // Skip nodes that are not connected to at least two segments
+        match node.segments.len() {
+            1 => continue,
+            2 => {
+                let seg_in = node.segments[0];
+                let seg_out = node.segments[1];
+
+                let seg_in_vec = antenna.segments[seg_in].unit_vector;
+                let seg_out_vec = antenna.segments[seg_out].unit_vector;
+
+                let total_length = antenna.segments[seg_in].length + antenna.segments[seg_out].length;
+
+                pulses.push(Pulse {
+                    center_node: i,
+                    seg_in,
+                    seg_out,
+                    total_length,
+                    unit_in: seg_in_vec,
+                    unit_out: seg_out_vec,
+                });
+            },
+            _ => {
+
+            }
         }
+
+
     }
 
 
