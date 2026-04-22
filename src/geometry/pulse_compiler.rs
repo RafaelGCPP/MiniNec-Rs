16ab774ba91a6b30cdb1602c3350c32d01e0e9e4
Rafael Pinto <rafaelgcpp@gmail.com>
2026-04-21 21:09:51 -0300

    pulse compiler

--- src/geometry/pulse_compiler.rs
+++ src/geometry/pulse_compiler.rs
@@ -6,35 +6,50 @@ pub fn compile_pulses(antenna: &Antenna) -> Vec<Pulse> {
     let nodes = &antenna.nodes;
 
     for (i, node) in nodes.iter().enumerate() {
-        match node.segments.len() {
-            1 => continue,
-            2 => {
-                let seg_in = node.segments[0];
-                let seg_out = node.segments[1];
+        let n_segs=node.segments.len();
 
-                let seg_in_vec = antenna.segments[seg_in].unit_vector;
-                let seg_out_vec = antenna.segments[seg_out].unit_vector;
-
-                let total_length = antenna.segments[seg_in].length + antenna.segments[seg_out].length;
-
-                pulses.push(Pulse {
-                    center_node: i,
-                    seg_in,
-                    seg_out,
-                    total_length,
-                    unit_in: seg_in_vec,
-                    unit_out: seg_out_vec,
-                });
-            },
-            _ => {
-
-            }
+        if  n_segs == 1 {
+            continue;
         }
 
+        let seg_in = node.segments[0];
+        let center_node = i;
 
+        // we need unit_in to point TO center_node
+        let unit_in = {
+            if antenna.segments[seg_in].nodes.1 == i {
+                antenna.segments[seg_in].unit_vector
+            } else {
+                -antenna.segments[seg_in].unit_vector
+            }
+        };
+
+        let segin_len= antenna.segments[seg_in].length;
+
+        for seg_idx in 1..n_segs+1 {
+
+            // all segments must leave center_node
+            let seg_out = node.segments[seg_idx];
+            let unit_out = {
+                if antenna.segments[seg_out].nodes.0 ==i {
+                    antenna.segments[seg_out].unit_vector
+                } else {
+                    -antenna.segments[seg_out].unit_vector
+                }
+            };
+
+            let segout_len=antenna.segments[seg_out].length;
+            let total_length=(segin_len+segout_len)/2.0;
+
+            pulses.push( Pulse {
+                center_node,
+                seg_in,
+                seg_out,
+                total_length,
+                unit_in,
+                unit_out
+            })
+        }
     }
-
-
-
     pulses
 }
